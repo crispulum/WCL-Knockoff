c79ef151ea898736b12d4168c70f09b5d335a39b
crispulum <crispulum@gmail.com>
2023-06-29 02:39:13 -0500

    added parsing data functionality. did not fix the swing issue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-function openLog() {
+function openLog(callback) {
 
     //inputs: the file name, I guess?
     //outputs: an array of objects
@@ -6,22 +6,111 @@ function openLog() {
     const fs = require('fs')
     const results = [];
 
-    fs.createReadStream('testshort.csv')
-        .pipe(csv(['Time/Type', 'PlayerID', 'SourceName', 'SourceFlags', 'SourceRaidFlag', 'TargetID', 'TargetName', 'TargetFlags', 'TargetRaidFlags', 'SpellID', 'SpellName', 'spellSchool', 'amount', 'p15', 'p16', 'p17', 'p18', 'p19', 'p20', 'p21', 'p22', 'p23', 'p24']))
+    fs.createReadStream('longlog.csv')
+        .pipe(csv(['TimeType', 'PlayerID', 'SourceName', 'SourceFlags', 'SourceRaidFlag', 'TargetID', 'TargetName', 'TargetFlags', 'TargetRaidFlags', 'SpellID', 'SpellName', 'spellSchool', 'p14', 'p15', 'p16', 'p17', 'p18', 'p19', 'p20', 'p21', 'p22', 'p23', 'p24'
+            , 'p25', 'p26', 'p27', 'mapID', 'p29', 'p30', 'amount', 'rawAmount', 'overkill', 'school', 'resisted', 'blocked', 'absorbed', 'critical', 'glancing', 'crushing']))
         .on('data', (data) => results.push(data))
         .on('end', () => {
-            console.log(results);
+            //console.log(results);
+            return callback(results);
         });
-}
-
-openLog();
-
-function parseEncounter() {
-    //inputs: an event log corresponding to a single encounter
-    //outputs: the parsed data
 
 
 }
+
+
+
+
+
+//openLog();
+
+
+/* function myOpenLog() {
+
+    const fs = require('fs');
+
+    fs.readFile('/home/ari/WCL-Knockoff/src/testlog.csv', 'utf8', (err, data) => {
+        if (err) throw err;
+
+        processCSVData(data);
+    });
+}
+
+
+function processCSVData(csvData) {
+
+    const lines = csvData.split('\n');
+    const headers = ['Time/Type', 'PlayerID', 'SourceName', 'SourceFlags', 'SourceRaidFlag', 'TargetID', 'TargetName', 'TargetFlags', 'TargetRaidFlags', 'SpellID', 'SpellName', 'spellSchool', 'p14', 'p15', 'p16', 'p17', 'p18', 'p19', 'p20', 'p21', 'p22', 'p23', 'p24'
+    , 'p25', 'p26', 'p27', 'mapID', 'p29', 'p30', 'amount', 'rawAmount', 'overkill', 'school', 'resisted', 'blocked', 'absorbed', 'critical', 'glancing', 'crushing'];
+
+
+}
+myOpenLog();
+ */
+function parseEncounter(inputData) {
+    //inputs: an event log corresponding to a single encounter?
+    //outputs: the parsed data. an object?
+
+
+    const outputResults = {}
+    inputData.forEach((element => {
+
+
+        if (element['TimeType'].includes("DAMAGE")) {
+            //handle damage event
+
+            //if player already doesn't yet
+            if (outputResults[element['SourceName']] == undefined) {
+                outputResults[element['SourceName']] = { damage: 0, healing: 0, dead: false }
+            }
+            //if player does exist
+
+            //outputResults[element['SourceName']]
+            outputResults[element['SourceName']].damage += Number(element['amount']);
+            if (Number(element['overkill']) > 0) {
+
+                if (outputResults[element['TargetName']] == undefined) {
+                    outputResults[element['TargetName']] = { damage: 0, healing: 0, dead: true }
+                }
+                //if player does exist
+                else {
+
+                    outputResults[element['TargetName']].dead = true;
+                }
+
+
+            }
+        }
+        //check if it killed someone
+
+
+
+        if (element['TimeType'].includes("HEAL")) {
+            //handle healing event
+
+            //if player already doesn't yet
+            if (outputResults[element['SourceName']] == undefined) {
+                outputResults[element['SourceName']] = { damage: 0, healing: 0, dead: false }
+            }
+            //if player does exist
+            else {
+
+                //outputResults[element['SourceName']]
+                outputResults[element['SourceName']].healing += Number(element['amount']);
+            }
+
+        }
+
+
+    }))
+    //console.log(nameArray)
+    console.log(outputResults);
+    return outputResults;
+
+}
+
+//openLog(parseEncounter);
+console.log(openLog(parseEncounter) + " is main CL")
 
 
 function visualizeData() {
@@ -31,17 +120,8 @@ function visualizeData() {
 
 }
 
-function getNextEvent() {
-    //inputs: the wcl file somehow lol
-    //outputs: the next line from the file? I probably can't afford to copy the entire thing into memory
 
 
-}
-
-function parseEvent() {
-    //inputs: a single event line
-    //outputs: probably nothing? except it will modify the objects whose actors are involved
-}
 
 function categorizeEvent() {
     //inputs: an event 
@@ -56,7 +136,6 @@ function getActor() {
 }
 
 function getValue() {
-
     //inputs: an event
     //outputs: the damage or healing amount corresponding to the event
 }
